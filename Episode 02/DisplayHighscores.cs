1b0be11a6c5e0098b7a45b06d170569eb1980aa9
Sebastian Lague <seblague@yahoo.com>
2015-04-15 07:23:38 +0200

    Episodes 01 and 02

---
+++ Episode 02/DisplayHighscores.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayHighscores : MonoBehaviour {
+
+	public Text[] highscoreFields;
+	Highscores highscoresManager;
+
+	void Start() {
+		for (int i = 0; i < highscoreFields.Length; i ++) {
+			highscoreFields[i].text = i+1 + ". Fetching...";
+		}
+
+				
+		highscoresManager = GetComponent<Highscores>();
+		StartCoroutine("RefreshHighscores");
+	}
+	
+	public void OnHighscoresDownloaded(Highscore[] highscoreList) {
+		for (int i =0; i < highscoreFields.Length; i ++) {
+			highscoreFields[i].text = i+1 + ". ";
+			if (i < highscoreList.Length) {
+				highscoreFields[i].text += highscoreList[i].username + " - " + highscoreList[i].score;
+			}
+		}
+	}
+	
+	IEnumerator RefreshHighscores() {
+		while (true) {
+			highscoresManager.DownloadHighscores();
+			yield return new WaitForSeconds(30);
+		}
+	}
+}
---
+++ Episode 02/Highscores.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highscores : MonoBehaviour {
+
+	const string privateCode = "4fcZoIDMO02emPvesFklzgGn_thjIN8EKfi4LNMXMcdw";
+	const string publicCode = "548c04c96e51b60c740bcdec";
+	const string webURL = "http://dreamlo.com/lb/";
+
+	DisplayHighscores highscoreDisplay;
+	public Highscore[] highscoresList;
+	
+	void Awake() {
+		highscoreDisplay = GetComponent<DisplayHighscores> ();
+	}
+
+	public void AddNewHighscore(string username, int score) {
+		StartCoroutine(UploadNewHighscore(username,score));
+	}
+
+	IEnumerator UploadNewHighscore(string username, int score) {
+		WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+		yield return www;
+
+		if (string.IsNullOrEmpty(www.error))
+			print ("Upload Successful");
+		else {
+			print ("Error uploading: " + www.error);
+		}
+	}
+
+	public void DownloadHighscores() {
+		StartCoroutine("DownloadHighscoresFromDatabase");
+	}
+
+	IEnumerator DownloadHighscoresFromDatabase() {
+		WWW www = new WWW(webURL + publicCode + "/pipe/");
+		yield return www;
+		
+		if (string.IsNullOrEmpty (www.error)) {
+			FormatHighscores (www.text);
+			highscoreDisplay.OnHighscoresDownloaded(highscoresList);
+		}
+		else {
+			print ("Error Downloading: " + www.error);
+		}
+	}
+
+	void FormatHighscores(string textStream) {
+		string[] entries = textStream.Split(new char[] {'\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+		highscoresList = new Highscore[entries.Length];
+
+		for (int i = 0; i <entries.Length; i ++) {
+			string[] entryInfo = entries[i].Split(new char[] {'|'});
+			string username = entryInfo[0];
+			int score = int.Parse(entryInfo[1]);
+			highscoresList[i] = new Highscore(username,score);
+			print (highscoresList[i].username + ": " + highscoresList[i].score);
+		}
+	}
+
+}
+
+public struct Highscore {
+	public string username;
+	public int score;
+
+	public Highscore(string _username, int _score) {
+		username = _username;
+		score = _score;
+	}
+
+}
